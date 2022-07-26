7d35ea1067094d11e18cbd7cbf60166b0307751c
ConnorBieszk <ConnorBieszk1@gmail.com>
2022-07-26 16:17:15 -0500

    Fixed Splash Screen

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 let intro = document.querySelector(".intro");
-let logo = document.querySelector(".logo-header")
-let logoSpan = document.querySelectorAll(".logo")
-
+let logo = document.querySelector("h1logo")
 window.addEventListener("DOMContentLoaded", ()=>{
-
     setTimeout(()=>{
-
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(()=>{
-                span.classList.add('active');
-            }, (idx + 1)*400)
-        });
-
-        setTimeout(()=>{
-            logoSpan.forEach((span, idx)=>{
-                setTimeout(()=>{
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1)*50)
-            })
-        })
-    },2000)
-
-
+        intro.style.top = '50vh';
+    }, 1500)
+});
+window.addEventListener("DOMContentLoaded", ()=>{
     setTimeout(()=>{
         intro.style.top = '-100vh';
-    }, 2300)
+    }, 2500)
 })
