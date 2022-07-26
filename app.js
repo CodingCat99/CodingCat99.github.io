a79ad57fb3863b57aba2e5c7d6d5c17d9980d9aa
ConnorBieszk <ConnorBieszk1@gmail.com>
2022-07-26 16:22:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ let logo = document.querySelector("h1logo")
 window.addEventListener("DOMContentLoaded", ()=>{
     setTimeout(()=>{
         intro.style.top = '50vh';
-    }, 1500)
+    }, 500)
 });
 window.addEventListener("DOMContentLoaded", ()=>{
     setTimeout(()=>{
-        intro.style.top = '-100vh';
+        intro.style.top = '75vh';
+    }, 1500)
+})
+window.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(()=>{
+        intro.style.top = '25vh';
     }, 2500)
+})
+window.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(()=>{
+        intro.style.top = '100vh';
+    }, 3500)
 })
