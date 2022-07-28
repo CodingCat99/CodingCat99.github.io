62e684d36b45404cc593df35b939f0ad662b0373
ConnorBieszk <84150127+CodingCat99@users.noreply.github.com>
2022-07-28 13:41:24 -0500

    Removed splash screen. Replaced with loading screen
    Some of the code used is from W3Schools.com

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-let intro = document.querySelector(".intro");
-let logo = document.querySelector("h1logo")
-window.addEventListener("DOMContentLoaded", ()=>{
-    setTimeout(()=>{
-        intro.style.top = '50vh';
-    }, 2500)
-});
-window.addEventListener("DOMContentLoaded", ()=>{
-    setTimeout(()=>{
-        intro.style.top = '75vh';
-    }, 3500)
-})
-window.addEventListener("DOMContentLoaded", ()=>{
-    setTimeout(()=>{
-        intro.style.top = '25vh';
-    }, 4500)
-})
-window.addEventListener("DOMContentLoaded", ()=>{
-    setTimeout(()=>{
-        intro.style.top = '100vh';
-    }, 5500)
-})
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
