f680ccbfc6df2d5a04667617fce1f020a52d77b2
ConnorBieszk <84150127+CodingCat99@users.noreply.github.com>
2022-07-28 13:55:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var myVar;
 console.log("Website Launched")
 function myFunction() {
   myVar = setTimeout(showPage, 3000);
-  console.log("Loading Complete");
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
+  console.log("Loading Complete");
 }
